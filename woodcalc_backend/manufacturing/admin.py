a6b57c4d26f42d0ee63b283786e0c39b81cb3276
Ahmad Abuhmaid <ahmadabuhmaid@hotmail.com>
2026-07-25 12:35:12 +0300

    Register cutting optimization models in Django admin

--- woodcalc_backend/manufacturing/admin.py
+++ woodcalc_backend/manufacturing/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from .models import ProductionStation, WorkOrder, WorkOrderItem, StationLog, MaterialConsumption
+from .models import StockSheet, CuttingJob, CuttingPart, CuttingLayout, PartPlacement
 
 admin.site.register(ProductionStation)
 admin.site.register(WorkOrder)
 admin.site.register(WorkOrderItem)
 admin.site.register(StationLog)
 admin.site.register(MaterialConsumption)
+admin.site.register(StockSheet)
+admin.site.register(CuttingJob)
+admin.site.register(CuttingPart)
+admin.site.register(CuttingLayout)
+admin.site.register(PartPlacement)
