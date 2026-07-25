13615b03d4a95d95e6ba094d292d5cf765776297
Ahmad Abuhmaid <ahmadabuhmaid@hotmail.com>
2026-07-25 23:13:41 +0300

    Remove textures route from urls.py

--- woodcalc_backend/inventory/urls.py
+++ woodcalc_backend/inventory/urls.py
@@ -1,11 +1,10 @@
 from rest_framework.routers import DefaultRouter
-from .views import MaterialViewSet, SupplierViewSet, StockMovementViewSet, StockAlertViewSet, MaterialTextureViewSet, DrawerSystemViewSet, SinkViewSet
+from .views import MaterialViewSet, SupplierViewSet, StockMovementViewSet, StockAlertViewSet, DrawerSystemViewSet, SinkViewSet
 router = DefaultRouter()
 router.register(r'materials', MaterialViewSet, basename='material')
 router.register(r'suppliers', SupplierViewSet, basename='supplier')
 router.register(r'movements', StockMovementViewSet, basename='stockmovement')
 router.register(r'alerts', StockAlertViewSet, basename='stockalert')
-router.register(r'textures', MaterialTextureViewSet, basename='materialtexture')
 router.register(r'drawer-systems', DrawerSystemViewSet, basename='drawersystem')
 router.register(r'sinks', SinkViewSet, basename='sink')
 urlpatterns = router.urls
