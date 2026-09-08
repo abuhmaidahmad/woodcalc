30fad3b6e9d8f2e7f7e6dbaa2c1b123e2242dce7
Ahmad Abuhmaid <ahmadabuhmaid@hotmail.com>
2026-09-08 19:39:40 +0300

    Tenant-scope hr viewsets

--- woodcalc_backend/hr/serializers.py
+++ woodcalc_backend/hr/serializers.py
@@ -6,6 +6,7 @@ class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
         fields = '__all__'
+        extra_kwargs = {'tenant': {'read_only': True}}
 
 
 class AttendanceSerializer(serializers.ModelSerializer):
--- woodcalc_backend/hr/views.py
+++ woodcalc_backend/hr/views.py
@@ -1,28 +1,33 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from tenants.mixins import TenantScopedMixin
+from tenants.permissions import HasActiveCompany
 from .models import Employee, Attendance, LeaveRequest, Payroll
 from .serializers import EmployeeSerializer, AttendanceSerializer, LeaveRequestSerializer, PayrollSerializer
 
 
-class EmployeeViewSet(ModelViewSet):
+class EmployeeViewSet(TenantScopedMixin, ModelViewSet):
     queryset = Employee.objects.all().order_by('first_name')
     serializer_class = EmployeeSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, HasActiveCompany]
 
 
-class AttendanceViewSet(ModelViewSet):
+class AttendanceViewSet(TenantScopedMixin, ModelViewSet):
+    tenant_filter_field = 'employee__tenant'
     queryset = Attendance.objects.all().order_by('-date')
     serializer_class = AttendanceSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, HasActiveCompany]
 
 
-class LeaveRequestViewSet(ModelViewSet):
+class LeaveRequestViewSet(TenantScopedMixin, ModelViewSet):
+    tenant_filter_field = 'employee__tenant'
     queryset = LeaveRequest.objects.all().order_by('-start_date')
     serializer_class = LeaveRequestSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, HasActiveCompany]
 
 
-class PayrollViewSet(ModelViewSet):
+class PayrollViewSet(TenantScopedMixin, ModelViewSet):
+    tenant_filter_field = 'employee__tenant'
     queryset = Payroll.objects.all().order_by('-period')
     serializer_class = PayrollSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, HasActiveCompany]
