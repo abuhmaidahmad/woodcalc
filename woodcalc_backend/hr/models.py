0e592bd1a12d519c01c3f396ea1fc5e271e381c4
Ahmad Abuhmaid <ahmadabuhmaid@hotmail.com>
2026-09-08 18:03:11 +0300

    Add tenant FK to Employee

--- woodcalc_backend/hr/models.py
+++ woodcalc_backend/hr/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from tenants.models import Company
 
 
 class Employee(models.Model):
+    tenant = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='employees', null=True, blank=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     job_title = models.CharField(max_length=100, blank=True)
